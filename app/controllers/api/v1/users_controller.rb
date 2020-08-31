0b77fa43362ba665f1a13d720ffba1454c37e1a0
Michał Kosyk <michal.kosyk@ragnarson.com>
2020-08-31 15:59:53 +0200

    Authenticate user requests

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,34 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      class NoAuthenticationHeaderError < StandardError; end
+
+      before_action :authenticate_user!
+
+      def profile
+        render json: @user
+      end
+
+      private
+
+        def request_headers
+          request.headers
+        end
+
+        def http_auth_header
+          raise NoAuthenticationHeaderError if request_headers["Authorisation"].nil?
+          request_headers["Authorisation"].split(' ').last
+        end
+
+        def authenticate_user!
+          @user = authentication_strategy.authenticate(http_auth_header)
+        rescue NoAuthenticationHeaderError, ::OpenFoodBank::Users::Authentication::UnknownTokenError
+          render status: :unauthorized
+        end
+
+        def authentication_strategy
+          @authentication_strategy ||= ::OpenFoodBank::Users::Authentication::MockedStrategy.new
+        end
+    end
+  end
+end
