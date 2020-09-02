a3639f764ce6111dbc0a3cb1d63062e3dc5b9450
Michał Kosyk <michal.kosyk@ragnarson.com>
2020-09-02 13:28:49 +0200

    authorise service provider user

---
+++ app/controllers/api/v1/service_providers_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class ServiceProvidersController < UsersController
+      before_action :authorise_service_provider!
+
+      attr_reader :service_provider
+
+      def authorised
+        render json: service_provider
+      end
+
+      private
+        def authorise_service_provider!
+          render status: :forbidden unless user.service_provider?
+
+          @service_provider = user
+        end
+    end
+  end
+end
+
