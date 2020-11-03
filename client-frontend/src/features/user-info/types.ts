ce8e3d4cdb71814f350e37eeb7043026a2d84fe7
Mikołaj Koropecki <mikolaj.koropecki@ragnarson.com>
2020-11-03 16:06:24 +0100

    Add user info slice with mocked data

---
+++ client-frontend/src/features/user-info/types.ts
@@ -0,0 +1,3 @@
+export interface IUserInfo {
+	role: string
+}
--- client-frontend/src/store/root-reducer.ts
+++ client-frontend/src/store/root-reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import shippingRequests from '../features/shipping-requests-container/redux-slice'
 import activeShippingRequest from '../features/active-shipping-request/redux-slice'
+import userInfo from '../features/user-info/redux-slice'
 
 const rootReducer = combineReducers({
 	shippingRequests,
 	activeShippingRequest,
+	userInfo,
 })
 
 export default rootReducer
